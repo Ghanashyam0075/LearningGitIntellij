617ea0b9ea6dc6793291f1da8f75c4332f0e754e
Ghanashyam Vibhandik <103465065+Ghanashyam0075@users.noreply.github.com>
2024-12-29 13:32:42 -0500

    Update DevClass.java
    Added more changes

--- src/main/java/DevClass.java
+++ src/main/java/DevClass.java
@@ -1,6 +1,6 @@
 public class DevClass {
     public static void main(String[] args) {
-        System.out.println("Hello - World!");
+        System.out.println("Hello - World! 123");
         //checking from Stream 2 Learn
     }
 }
